f21b2706a08cfc932b0a7de05716f019acec29a6
Joel Diez <joelricardodiezvillanueva@Joels-MacBook-Pro.local>
2021-02-14 18:50:44 -0500

    link navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,96 +1,143 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 import useTranslation from '../hooks/useTranslations'
+import Link from 'next/link'
 
 const Navigation = () => {
   const { t } = useTranslation()
+
+  const router = useRouter()
+
   return (
     <nav className="mainContainerNav">
       <ul className="navBar">
         <li className="navBar--active">
-          <a href="#">{t('index').toUpperCase()}</a>
+          <Link href={{ pathname: '/[lang]', query: { lang: router.query.lang } }}>
+            <a>{t('index').toUpperCase()}</a>
+          </Link>
         </li>
         <li>
-          <a href="#">{t('about').toUpperCase()}</a>
+          <Link href={{ pathname: '/[lang]/aboutus', query: { lang: router.query.lang } }}>
+            <a>{t('about').toUpperCase()}</a>
+          </Link>
           <ul className="navBar__subMenu">
             <li>
-              <a href="#">{t('congregationHistory')}</a>
+              <Link href={{ pathname: '/[lang]/aboutus/carisma', query: { lang: router.query.lang } }}>
+                <a>{t('ourCharisma')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('ourCharisma')}</a>
-            </li>
-            <li>
-              <a href="#">{t('provinces')}</a>
+              <Link href={{ pathname: '/[lang]/aboutus/province', query: { lang: router.query.lang } }}>
+                <a>{t('provinces')}</a>
+              </Link>
             </li>
           </ul>
         </li>
         <li>
-          <a href="#">{t('where').toUpperCase()}</a>
+          <Link href={{ pathname: '/[lang]/fraternity', query: { lang: router.query.lang } }}>
+            <a>{t('where').toUpperCase()}</a>
+          </Link>
         </li>
         <li>
-          <a href="#">{t('ourWork').toUpperCase()}</a>
+          <Link href={{ pathname: '/[lang]/our-work', query: { lang: router.query.lang } }}>
+            <a>{t('ourWork').toUpperCase()}</a>
+          </Link>
           <ul className="navBar__subMenu">
             <li>
-              <a href="#">{t('misions')}</a>
+              <Link href={{ pathname: '/[lang]/our-work/mision', query: { lang: router.query.lang } }}>
+                <a>{t('misions')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('educational')}</a>
+              <Link href={{ pathname: '/[lang]/our-work/educational', query: { lang: router.query.lang } }}>
+                <a>{t('educational')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('parochial')}</a>
+              <Link href={{ pathname: '/[lang]/our-work/parroquial', query: { lang: router.query.lang } }}>
+                <a>{t('parochial')}</a>
+              </Link>
             </li>
           </ul>
         </li>
         <li>
-          <a href="#">{t('beFranciscan').toUpperCase()}</a>
+          <Link href={{ pathname: '/[lang]/be-franciscan', query: { lang: router.query.lang } }}>
+            <a>{t('beFranciscan').toUpperCase()}</a>
+          </Link>
           <ul className="navBar__subMenu">
             <li>
-              <a href="#">{t('vocationMinistry')}</a>
+              <Link href={{ pathname: '/[lang]/be-franciscan/aspirantate', query: { lang: router.query.lang } }}>
+                <a>{t('aspirantate')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('aspirantate')}</a>
+              <Link href={{ pathname: '/[lang]/be-franciscan/postulancy', query: { lang: router.query.lang } }}>
+                <a>{t('postulancy')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('postulancy')}</a>
+              <Link href={{ pathname: '/[lang]/be-franciscan/novitiate', query: { lang: router.query.lang } }}>
+                <a>{t('novitiate')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('novitiate')}</a>
+              <Link href={{ pathname: '/[lang]/be-franciscan/junerado', query: { lang: router.query.lang } }}>
+                <a>{t('junerado')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('junerado')}</a>
+              <Link href={{ pathname: '/[lang]/be-franciscan/permanent-education', query: { lang: router.query.lang } }}>
+                <a>{t('permanentEducation')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('permanentEducation')}</a>
-            </li>
-            <li>
-              <a href="#">{t('layBranch')}</a>
+              <Link href={{ pathname: '/[lang]/be-franciscan/lay-branch', query: { lang: router.query.lang } }}>
+                <a>{t('layBranch')}</a>
+              </Link>
             </li>
           </ul>
         </li>
         <li>
-          <a href="#">{t('spirituality').toUpperCase()}</a>
+          <Link href={{ pathname: '/[lang]/spirituality', query: { lang: router.query.lang } }}>
+            <a>{t('spirituality').toUpperCase()}</a>
+          </Link>
           <ul className="navBar__subMenu">
             <li>
-              <a href="#">{t('sanFranciscoDeAsis')}</a>
+              <Link href={{ pathname: '/[lang]/spirituality/san-francisco-de-asis', query: { lang: router.query.lang } }}>
+                <a>{t('sanFranciscoDeAsis')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('santaClaraDeAsis')}</a>
+              <Link href={{ pathname: '/[lang]/spirituality/santa-clara-de-asis', query: { lang: router.query.lang } }}>
+                <a>{t('santaClaraDeAsis')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('madreCaridadBraderZanher')}</a>
+              <Link href={{ pathname: '/[lang]/spirituality/madre-caridad-brader-zanher', query: { lang: router.query.lang } }}>
+                <a>{t('madreCaridadBraderZanher')}</a>
+              </Link>
             </li>
           </ul>
         </li>
         <li>
-          <a href="#">{t('eucharisticSoul').toUpperCase()}</a>
+          <Link href={{ pathname: '/[lang]/eucharistic-soul', query: { lang: router.query.lang } }}>
+            <a>{t('eucharisticSoul').toUpperCase()}</a>
+          </Link>
           <ul className="navBar__subMenu">
             <li>
-              <a href="#">{t('eucharisticSpirituality')}</a>
+              <Link href={{ pathname: '/[lang]/eucharistic-soul/eucharistic-spirituality', query: { lang: router.query.lang } }}>
+                <a>{t('eucharisticSpirituality')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('perpetualAdoration')}</a>
+              <Link href={{ pathname: '/[lang]/eucharistic-soul/perpetual-adoration', query: { lang: router.query.lang } }}>
+                <a>{t('perpetualAdoration')}</a>
+              </Link>
             </li>
             <li>
-              <a href="#">{t('santuario')}</a>
+              <Link href={{ pathname: '/[lang]/eucharistic-soul/santuario', query: { lang: router.query.lang } }}>
+                <a>{t('santuario')}</a>
+              </Link>
             </li>
           </ul>
         </li>
