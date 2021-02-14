08c41cbe7bded86dd0d98e116096b1565a14679b
Bryan Pardo <alex_fajar@hotmail.com>
2021-02-14 11:20:39 -0500

    Permanent Education

---
+++ components/Screens/PEducationScreen.tsx
@@ -0,0 +1,27 @@
+//i18ns
+import Header from '../header'
+import CardImage from '../CardImage'
+
+const AspirantateScreen = () => {
+    return (
+        <>
+            <Header image={'/images/franciscan/franciscan.png'}>
+                <>
+                    <div className="main__section">
+                        <div className="franciscan__index">
+                            <div className="secondary__title">
+                                <h1>FORMACIÓN PERMANENTE</h1>
+                            </div>
+                            <div className="half__card" style={{ width: '80%', margin: 'auto' }}>
+                                <CardImage title="REFRAMI" img={'/images/fraternity/fraternity.png'} />
+                                <CardImage title="MIFRAMI" />
+                            </div>
+                        </div>
+                    </div>
+                </>
+            </Header>
+        </>
+    )
+}
+
+export default AspirantateScreen
