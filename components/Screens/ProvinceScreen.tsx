2ecd1ffdb4d4229407cd83b045e7177b6a3d8280
johan <johanavilax@gmail.com>
2021-02-22 21:31:17 -0500

    provinces & fraternity

--- components/Screens/ProvinceScreen.tsx
+++ components/Screens/ProvinceScreen.tsx
@@ -1,15 +1,13 @@
 import client from '@/graphql/config'
 import { listProvinces } from '@/graphql/queries'
-import { PropsScreens, Province } from '@/types/types'
+import { Province } from '@/types/types'
 import { gql } from '@apollo/client'
 
 import React, { useEffect, useState } from 'react'
 import Header from '../header'
 
-const ProvinceScreen = (props: PropsScreens) => {
-  //   const [provinces, setProvinces] = useState<Province[]>()
+const ProvinceScreen = () => {
   const [itemsSlider, setItemsSlider] = useState<Province[]>()
-  //   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     getData()
@@ -21,6 +19,7 @@ const ProvinceScreen = (props: PropsScreens) => {
     try {
       await client.cache.reset()
       const res = (await client.query({ query: gql(listProvinces) })) as { data: { listProvinces: Province[] } }
+      console.log(res)
       setItemsSlider(res.data.listProvinces)
     } catch (error) {
       console.log(error)
@@ -31,16 +30,19 @@ const ProvinceScreen = (props: PropsScreens) => {
     <Header>
       <>
         {itemsSlider?.map((e, i) => (
-          <div className="main__section">
+          <div key={i} className="main__section">
             <div className="province__container">
               <div className="secondary__title">
                 <h1>{e.name}</h1>
               </div>
               <div className="carousel__provinces">
-                <div key={i}>
+                <div>
                   <div className="item">
                     <div className="mask"></div>
-                    <img src="/images/provinces/province.jpg" alt="CONGREGACIÓN DE FRANCISCANAS DE MARIA INMACULADA" />
+                    <img
+                      src={e.photo?.key ? e.photo?.key : '/images/provinces/province.jpg'}
+                      alt="CONGREGACIÓN DE FRANCISCANAS DE MARIA INMACULADA"
+                    />
                   </div>
                 </div>
               </div>
