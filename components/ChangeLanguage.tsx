3567584cb5a04d9d3a6f0afbf0b540db1ba8989f
johan <johanavilax@gmail.com>
2021-02-22 21:01:06 -0500

    all lenguage

--- components/ChangeLanguage.tsx
+++ components/ChangeLanguage.tsx
@@ -15,9 +15,9 @@ const ChangeLanguage = () => {
     <div className="body__content__flags">
       <Image onClick={() => changeLanguage('es')} width={40} height={40} src="/images/body/custom-icons/spain.svg" />
       <Image onClick={() => changeLanguage('en')} width={40} height={40} src="/images/body/custom-icons/united-kingdom.svg" />
-      <Image onClick={() => changeLanguage('es')} width={40} height={40} src="/images/body/custom-icons/france.svg" />
-      <Image onClick={() => changeLanguage('es')} width={40} height={40} src="/images/body/custom-icons/germany.svg" />
-      <Image onClick={() => changeLanguage('es')} width={40} height={40} src="/images/body/custom-icons/italy.svg" />
+      <Image onClick={() => changeLanguage('fr')} width={40} height={40} src="/images/body/custom-icons/france.svg" />
+      <Image onClick={() => changeLanguage('de')} width={40} height={40} src="/images/body/custom-icons/germany.svg" />
+      <Image onClick={() => changeLanguage('it')} width={40} height={40} src="/images/body/custom-icons/italy.svg" />
     </div>
   )
 }
