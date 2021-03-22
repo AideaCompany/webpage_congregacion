9740d95c1c401c8e76746d8e99aca1a8d06740d1
Joel Diez <joelricardodiezvillanueva@Joels-MacBook-Pro.local>
2021-03-22 14:44:35 -0500

    metadata

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,6 +14,11 @@ const Layout = (props: propsLayout) => {
       <Head>
         <title>{`Francicanas de María Inmaculada  | ${title}`}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Congregación de Franciscanas de María Inmaculada" />
+        <meta name="author" content="Aidea S.A.S." />
+        <meta property="og:title" content="Francicanas de María Inmaculada" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Congregación de Franciscanas de María Inmaculada" />
       </Head>
       <main>
         <div className="mainContainer">
