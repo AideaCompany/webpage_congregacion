4ac8c8376f4fc806d61d04392e2c152dd185b1ba
Johan <johanavilax@gmail.com>
2021-04-30 17:38:35 -0500

    version

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 // next.config.js
-const withImages = require('next-images')
-module.exports = withImages({
-  webpack(config, options) {
-    return config
-  }
-})
 
 module.exports = {
   async rewrites() {
