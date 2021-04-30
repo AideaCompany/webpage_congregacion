326e8cbfb49eeae8d0d7f6905a8107c251cf421a
Joel Diez <joelricardodiezvillanueva@Joels-MacBook-Pro.local>
2021-04-30 17:44:24 -0500

    merge

--- next.config.js
+++ next.config.js
@@ -5,14 +5,3 @@ module.exports = withImages({
     return config
   }
 })
-
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/es'
-      }
-    ]
-  }
-}
