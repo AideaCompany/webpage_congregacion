f48cc676febb5c7c62356f9a4bb12fda7a90177e
Johan <johanavilax@gmail.com>
2021-04-30 19:03:44 -0500

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 // next.config.js
 const withImages = require('next-images')
-module.exports = withImages({
-  webpack(config, options) {
-    return config
+module.exports = {
+  ...withImages({
+    webpack(config, options) {
+      return config
+    }
+  }),
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/es'
+      }
+    ]
   }
-})
+}
